40de3cc3a2644630615af92392ae6a2e1668ce92
kimminkyu <kmk50009@naver.com>
2022-02-10 21:37:55 +0900

    Add Event Test

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import Output from "./Output";
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h1>Hello World!</h1>
-      <p>It's good to see you!</p>
+      {!changedText && <Output>It's good to see you!</Output>}
+      {changedText && <Output>Changed!</Output>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 };
